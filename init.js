a16916a1fdd32b3295177d8af68755dae6469975
Tom Scharstein <trscharstein@gmail.com>
2019-07-04 21:32:16 -0700

    Updating .env names to be more precise, adding template

--- init.js
+++ init.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import firestore from "firebase/firestore";
 export default firebase
   .initializeApp({
-    apiKey: process.env.VUE_APP_FIREBASE_KEY,
+    apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
     authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
     databaseURL: process.env.VUE_APP_FIREBASE_DATABASEURL,
-    projectId: process.env.VUE_APP_PROJECTID,
-    storageBucket: process.env.VUE_APP_FIREBASE_BUCKET,
-    messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
-    appId: process.env.VUE_APP_ID
+    projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.VUE_APP_FIREBASE_APPID
   })
   .firestore();
